fdc9c0ed243664591584746c06c439aacf053d75
chriscrat <barreto.christophe@gmail.com>
2014-06-10 02:10:20 +0200

    CBA - Dessin en temps réel + chargement dessin au chargement

--- services/user_service.js
+++ services/user_service.js
@@ -1,48 +1,49 @@
 var userService = {
-
     insert: function(data, _collection, callback){
-            var collection =  global.mongoDb.collection(_collection);
-            collection.insert(data, function(err, result) {
-                if(!err)
-                    console.log("Insert success");
-                else
-                    console.log("Error insert")});
-            callback();
-        },
-        getCountDrawing: function(_collection, callback){
-            var collection =  global.mongoDb.collection(_collection);
-            var count=0;
-            collection.distinct('id',function(err, docs){
-                if(!err){
-                    if(docs==undefined){
-                        global.collectionLength = count;
-                        callback();
-                    }
-                    else{
-                        global.collectionLength = docs.length;
-                        callback();
-                    }
-                }
-                else
-                {
+        var collection =  global.mongoDb.collection(_collection);
+        collection.insert(data, function(err, result) {
+            if(!err)
+                console.log("Insert success");
+            else
+                console.log("Error insert")});
+        callback();
+    },
+    getCountDrawing: function(_collection, callback){
+        var collection =  global.mongoDb.collection(_collection);
+        var count=0;
+        collection.distinct('id',function(err, docs){
+            if(!err){
+                if(docs==undefined){
+                    global.collectionLength = count;
                     callback();
                 }
-            });
-            callback();
-        },
-        getCollectionContentById: function(_collection, id, callback){
-            var collection =  global.mongoDb.collection(_collection);
-            collection.find({'id': id }).toArray(function(err, items) {
-                if(!err){
-                    global.content = items;
+                else{
+                    global.collectionLength = docs.length;
                     callback();
                 }
-                else
-                {
-                    console.log("Error during query request");
-                    callback();
-                }
-            });
-        }
+            }
+            else
+            {
+                callback();
+            }
+        });
+        callback();
+    },
+    getCollectionContentById: function(_collection, id, callback){
+        var collection =  global.mongoDb.collection(_collection);
+
+        collection.find({id: id}).toArray( function(err, items) {
+            if(!err){
+                global.content = items;
+                console.log("test "+items);
+                callback();
+            }
+            else
+            {
+                console.log("Error during query request");
+                callback();
+            }
+        });
+    }
 }
 module.exports=userService;
